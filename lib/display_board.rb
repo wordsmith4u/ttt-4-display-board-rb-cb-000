7653cf9cf8d03035552dbdd7bf58f4eddecad05f
Ronald Gerlach <wordsmith4u@hotmail.com>
2018-04-24 14:16:52 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,8 @@
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-
-def display_board(board)
-	puts " #{board[0]} | #{board[1]} | #{board[2]} "
-	puts "-----------"
-	puts " #{board[3]} | #{board[4]} | #{board[5]} "
-	puts "-----------"
-	puts " #{board[6]} | #{board[7]} | #{board[8]} "
+def display_board( board = [ "X", "", "", "", "X", "", "", "", ""] )
+  puts" #{board[0]} | #{board[1]} | #{board[2]} "
+  puts"-----------"
+  puts" #{board[3]} | #{board[4]} | #{board[5]} "
+  puts"-----------"
+  puts" #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-display_board(board)
+display_board

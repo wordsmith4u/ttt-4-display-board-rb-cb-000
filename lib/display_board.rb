f55eba342264e9e701bcfec2370ce6af27afb830
Ronald Gerlach <wordsmith4u@hotmail.com>
2018-04-24 14:34:03 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,10 @@
-def display_board( board = [ "X", "", "", "", "X", "", "", "", ""] )
-  puts" #{board[0]} | #{board[1]} | #{board[2]} "
-  puts"-----------"
-  puts" #{board[3]} | #{board[4]} | #{board[5]} "
-  puts"-----------"
-  puts" #{board[6]} | #{board[7]} | #{board[8]} "
+def display_board(board)
+separator = "|"
+lines = "-----------"
+
+puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+puts "#{lines}"
+puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts "#{lines}"
+puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
 end
-display_board
